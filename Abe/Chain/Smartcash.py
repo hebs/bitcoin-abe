93a4da2c22d3ec0519f740f5a2cf6ff30dfc14a8
hebs <hebs007wrx@gmail.com>
2018-03-19 12:46:45 -0500

    updated Smartcash.py with what dermoth suggested.
    Still getting merkle mismatch though.

--- Abe/Chain/Smartcash.py
+++ Abe/Chain/Smartcash.py
@@ -1,4 +1,4 @@
-# Copyright(C) 2014 by Abe developers.
+# Copyright(C) 2014-2018 by Abe developers.
 
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Affero General Public License as
@@ -14,16 +14,16 @@
 # License along with this program.  If not, see
 # <http://www.gnu.org/licenses/agpl.html>.
 
-#**thehebs 3/5/2018 Changed ZCoin to Smartcash on line 23
-
-from . import BaseChain
-from .. import util
-from pprint import pprint
+from .KeccakChain import KeccakChain
 
 class Smartcash(KeccakChain):
     """
-    A blockchain using 256-bit SHA3 (Keccak) as the block header hash.
+    Smartcash blockchain (https://smartcash.cc/)
     """
-    def block_header_hash(chain, header):
-        pprint (vars(chain))
-        return util.sha3_256(header)
+    def __init__(chain, **kwargs):
+        chain.name = 'Smartcash'
+        chain.code3 = 'SMT'
+        chain.address_version = '\x63'
+        chain.script_addr_vers = '\x18'
+        chain.magic = '\x5c\xa1\xab\x1e'
+        KeccakChain.__init__(chain, **kwargs)
